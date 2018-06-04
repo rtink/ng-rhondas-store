7568423ac832ac5498fd8952f8e150f409f76f0c
Rhonda Tinkham <rtinkham@live.com>
2018-06-03 20:30:38 -0400

    added components, server code for products, auth, categories, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   searchTerm = '';
   isCollapsed = true;
+
+  constructor(private router: Router, private data: DataService) {
+    this.data.getProfile();
+  }
 
   get token() {
     return localStorage.getItem('token');
@@ -21,7 +28,11 @@ export class AppComponent {
     dropdown.close();
   }
 
-  logout() {}
+  logout() {
+    this.data.user = {};
+    localStorage.clear();
+    this.router.navigate(['']);
+  }
 
   search() {}
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { NavigationStart, Router } from '@angular/router';
 
+import { RestApiService } from './rest-api.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +13,7 @@ export class DataService {
 
   user: any;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router, private rest: RestApiService) { 
     this.router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
         this.message = '';
@@ -32,5 +34,18 @@ export class DataService {
   warning(message) {
     this.messageType = 'warning';
     this.message = message;
+  }
+
+  async getProfile() {
+    try {
+      if (localStorage.getItem('token')) {
+        const data = await this.rest.get(
+        'http://localhost:3030/api/accounts/profile'
+        );
+        this.user = data['user'];
+      }
+    } catch (error) {
+      this.error(error);
+    }
   }
 }
